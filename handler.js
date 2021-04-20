a8aa2302e4df1cb600f1419a29c281de1d9cdfd9
Thiago Roberto <thiago.nemo4code@gmail.com>
2021-04-20 15:57:00 -0300

    adding readme

--- handler.js
+++ handler.js
@@ -1,18 +1,12 @@
-'use strict';
-
-module.exports.hello = async (event) => {
-  return {
+module.exports.hello = (event, context, callback) => {
+  callback(null, null);
+};
+module.exports.ciao = (event, context, callback) => {
+  const response = {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify({
+      message: "ciao world!",
+    }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  callback(null, response);
 };
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Step Functions Example
+
+### Deployment
+
+`serverless deploy -v -s dev`
+
+### Running
+
+`serverless invoke stepf --name hellostepfunc1 -s dev`
